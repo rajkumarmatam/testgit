bf1386a3253ecad8a9000a08aecd2b67216c7287
rmatamx <rmatamx@RMATAMX-MOBL>
2016-11-23 16:47:25 +0530

    testgit

---
+++ gittestproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gittestproject.Startup))]
+namespace gittestproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
